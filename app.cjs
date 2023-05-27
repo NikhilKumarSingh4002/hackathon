84bc412ce1d8ec6212eec2f299f90615b3ab777b
NikhilKumarSingh4002 <nkumarsingh625@gmail.com>
2023-05-27 07:33:38 +0530

    Initial commit

---
+++ app.cjs
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+var __dirname=path.resolve();
+const bodyParser=require('body-parser');
+app.set('view engine', 'ejs');
+
+
+app.use('/public', express.static('public'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+  res.render('home')
+});
+
+app.get('/register',(req,res)=>{
+  res.render('register');
+});
+
+app.get('/shopping',(req,res)=>{
+  res.render('home1');
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
